dd6e84bd7acc0154b1148202d449f00c7d849868
Agustin Salinas <chaucharian@gmail.com>
2024-11-02 12:47:06 -0300

    fix: correct secret file mapped out

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,8 @@
-// import * as firebase from './.keys/firebase.json';
-// import * as cloudinary from './.keys/cloudinary.json';
 import { getSecretsPath } from './getSecretsPath';
 
 export default () => {
   return {
-    // cloudinary,
-    // firebase,
-    cloudinary: require(getSecretsPath('firebase.json')),
+    cloudinary: require(getSecretsPath('cloudinary.json')),
     firebase: require(getSecretsPath('firebase.json')),
     TMDB_API_KEY: process.env.TMDB_API_KEY
   };
